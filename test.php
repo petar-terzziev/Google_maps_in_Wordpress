52476281c69359527afd0af3819b360cf111635b
Petar <petar.terzziev@gmail.com>
2018-10-12 17:44:13 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,21 +1,16 @@
 <?php
- $db = new PDO('mysql:host=localhost;dbname=coordinates','root');
+ $db = new PDO('mysql:host=localhost;dbname=phpmyadmin','phpmyadmin','1241323');
 $sql= "SELECT * FROM coordinates";
+
 $lats=array();
-$count= $db->query($sql)->fetchColumn();
-if($count<10000){
-	$file=file_get_contents("https://data.opendatasoft.com/api/records/1.0/search/?dataset=geonames-all-cities-with-a-population-1000%40public&rows=10000&facet=timezone&facet=country&refine.timezone=Asia%2FShanghai");
+	$file=file_get_contents("https://data.opendatasoft.com/api/records/1.0/search/?dataset=geonames-all-cities-with-a-population-1000%40public&rows=11&facet=timezone&facet=country&refine.timezone=Asia%2FShanghai");
 $cities=json_decode($file);
 foreach($cities->records as $city){
-$db->query('INSERT INTO coordinates (lat,lng) VALUES ('.$city->fields->coordinates[0].','.$city->fields->coordinates[1].')');
-$lats[]=$city->fields->coordinates;
+$db->query('INSERT IGNORE INTO coordinates (recordid,lat,lng) VALUES (\''.$city->recordid.'\','.$city->fields->coordinates[0].','.$city->fields->coordinates[1].')');
 }
-}
-else{
-   foreach($db->query($sql) as $row){
-   	$lats[]=$row;
-   }
+foreach($db->query($sql) as $row){
+	$lats[]=$row;
 }
 header('Content-Type: application/json');
-echo json_encode($lats);
+	echo json_encode($lats);
 ?> 
