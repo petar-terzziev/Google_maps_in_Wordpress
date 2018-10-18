e557ba9c00c7b35570629b9fc02c3af6691ed616
Petar <petar.terzziev@gmail.com>
2018-10-18 13:56:40 +0300

    trying to filter cities

--- maps/content.php
+++ maps/content.php
@@ -1,23 +1,15 @@
-<div class="blog-post">
-	<h2 class="blog-post-title"><?php the_title(); ?></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+
 <?php global $wpdb;
-  $asia=file_get_contents("https://data.opendatasoft.com/api/records/1.0/search/?dataset=geonames-all-cities-with-a-population-1000%40public&rows=10000&facet=timezone&facet=country&refine.timezone=Asia%2FShanghai");
-$asia_cities=json_decode($asia);
-foreach($asia_cities->records as $city){
-
-$sql=$wpdb->prepare("INSERT IGNORE  into wp_coordinates (recordid,lat,lng,country) values (%s,%d,%d,%s)",array($city->recordid,$city->fields->coordinates[0],$city->fields->coordinates[1],$city->fields->country));
-$wpdb->query($sql);
-}
-
-
+;
 $countries=$wpdb->get_results("select distinct country from wp_coordinates");
 $cur_country=$_POST['select_country'];
-$sql='select lat,lng from wp_coordinates where country=\''.$cur_country.'\'';
-$coordinates=$wpdb->get_results($sql);
+$sql_coordinates='select lat,lng from wp_coordinates where country=\''.$cur_country.'\'';
+$sql_cities='select city from wp_coordinates where country=\''.$cur_country.'\'';
+$cities=$wpdb->get_results($sql_cities);
+$coordinates=$wpdb->get_results($sql_coordinates);
 ?> <?php the_content(); ?>
 
-</div>
+<html>
   <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -38,6 +30,9 @@ $coordinates=$wpdb->get_results($sql);
       <select name="select_country" id="select_country" onchange="this.form.submit(); initMap()">
     <option value="null" id="default_option" selected>Select Country</option>
 </select>
+      <select name="select_city" id="select_city" onchange="this.form.submit(); initMap()">
+    <option value="null" id="default_option" selected>Select City</option>
+</select>
 </form> 
 </div>
     <!--The div element for the map -->
@@ -56,6 +51,11 @@ select_menu.add(option, select_menu[i+1]);
          }
 
      	}
+
+        function load_cities(){
+          console.log("was called");
+    
+      }
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
@@ -80,7 +80,7 @@ document.getElementById('default_option').value='<?php echo $cur_country?>';
 
 }
 }
-
+$(document).ready(load_cities());
 $(document).ready(load_countries());
 
     </script>
